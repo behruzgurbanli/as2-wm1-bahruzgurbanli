e3c80f00d7f3352cdeec3ee90e89a05123e29159
Behruz Gurbanli <behruzgurbanli@gmail.com>
2023-12-03 21:04:00 +0400

    gallery done

--- src/app.js
+++ src/app.js
@@ -45,7 +45,51 @@ const displayProducts = (productList) => {
                 <p>Stock: ${product.stock}</p>
                 <img src="${product.thumbnail}" alt="${product.title}" style="max-width: 200px; max-height: 200px;">
             `;
+      productElement.addEventListener("click", () => productPage(product));
 
       productListElement.appendChild(productElement);
     });
 }
+
+const productPage = (product) => {
+  const productPageContainer = document.createElement("div");
+  productPageContainer.classList.add("product-page");
+
+  const productDetails = document.createElement("div");
+  productDetails.innerHTML = `
+      <h2>${product.title}</h2>
+      <p>Price: ${product.price}</p>
+      <p>Discount: ${product.discountPercentage}%</p>
+      <p>Category: ${product.category}</p>
+      <p>Description: ${product.description}</p>
+      <p>Stock: ${product.stock}</p>
+      <img src="${product.thumbnail}" alt="${product.title}" style="max-width: 300px; max-height: 300px;">
+  `;
+
+  if (product.gallery && Array.isArray(product.gallery)) {
+    const productGallery = document.createElement("div");
+    productGallery.classList.add("gallery");
+
+    product.gallery.forEach((image) => {
+      const galleryImage = document.createElement("img");
+      galleryImage.src = image;
+      galleryImage.alt = product.title;
+      productGallery.appendChild(galleryImage);
+    });
+    productPageContainer.appendChild(productGallery);
+  }
+
+  
+  productPageContainer.appendChild(productDetails);
+  
+  const closeButton = document.createElement("button");
+  closeButton.innerHTML = "&times;";
+  closeButton.classList.add("close-button");
+  closeButton.addEventListener("click", () => {
+    document.body.removeChild(productPageContainer);
+  });
+
+  productPageContainer.appendChild(closeButton);
+  
+  document.body.appendChild(productPageContainer);
+};
